c0b1ee14eb6ba3603b483ebbf1a72d94ab71d0e0
silva01 <silva20102@gmail.com>
2023-08-21 21:07:09 -0300

    Removed imports not used

--- src/main/java/br/net/silva/daniel/generator/password/key/keypasswordgenerator/domain/password/interfaces/AbstractKeyGenerator.java
+++ src/main/java/br/net/silva/daniel/generator/password/key/keypasswordgenerator/domain/password/interfaces/AbstractKeyGenerator.java
@@ -11,19 +11,6 @@ public abstract class AbstractKeyGenerator {
     protected Integer qtdCharacters;
     private final Random random;
 
-    private static final char[] ALPHA_UPPER_CHARACTERS = { 'A', 'B', 'C', 'D',
-            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-            'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-    private static final char[] ALPHA_LOWER_CHARACTERS = { 'a', 'b', 'c', 'd',
-            'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-    private static final char[] NUMERIC_CHARACTERS = { '0', '1', '2', '3', '4',
-            '5', '6', '7', '8', '9' };
-    private static final char[] SPECIAL_CHARACTERS = { '~', '`', '!', '@', '#',
-            '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{',
-            ']', '}', '\\', '|', ';', ':', '\'', '"', ',', '<', '.', '>', '/',
-            '?' };
-
     protected AbstractKeyGenerator(boolean haveUpperCase, boolean haveLowerCase, boolean haveNumeric, Integer qtdCharacters, boolean haveSpecialCharacter) {
         this.haveUpperCase = haveUpperCase;
         this.haveLowerCase = haveLowerCase;
